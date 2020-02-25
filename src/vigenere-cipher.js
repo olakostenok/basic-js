4123ee76e0b63dc105072272b50e897df165ef9b
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-25 21:32:32 +0300

    feat: add solution for 10th task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,53 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    
+    constructor(isDirect = true) {
+        this.isDirect = isDirect;
+    }
+    
+    encrypt(message, key) {
+        if(!message || !key)
+            throw new Error();
+        
+        const KEY = key.toUpperCase();
+        var result = '';
+        var i = -1;
+        
+        for(var letter of message.toUpperCase()) {
+            if( /^[^a-z]{1}$/i.test(letter) )
+                result += letter;
+            else
+                result += String.fromCharCode(
+                    (letter.charCodeAt(0) + KEY.charCodeAt(++i % KEY.length) - 130) % 26 + 65 
+                );
+        }
+        
+        return result;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        if(!message || !key)
+            throw new Error();
+        
+        const KEY = key.toUpperCase();
+        var result = '';
+        var i = -1;
+        
+        if(!this.isDirect)
+            message = message.split('').reverse().join('');
+        
+        for(var letter of message.toUpperCase()) {
+            if( /^[^a-z]{1}$/i.test(letter) )
+                result += letter;
+            else
+                result += String.fromCharCode(
+                    (letter.charCodeAt(0) - KEY.charCodeAt(++i % KEY.length) + 26) % 26 + 65 
+                );
+        }
+        
+        if(!this.isDirect)
+            result = result.split('').reverse().join('');
+        
+        return result;
     }
 }
 
