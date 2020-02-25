f6af319b5c59fc63bb9a71fccf5acb3f87feda6d
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-25 16:30:32 +0300

    feat: add solution for 2nd task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if (!Array.isArray(members)) return false;
+  return members.reduce((arr, item) => {
+    if (typeof item === 'string') arr.push(item.trim()[0].toUpperCase());
+    return arr;
+  }, []).sort().join('');
+
 };
