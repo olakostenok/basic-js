a0cd510264059a3d11d2589ac964a4ebd44cd4ea
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-25 18:25:39 +0300

    feat: add solution for 6th task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    if (!Array.isArray(arr)) throw "Error";
+
+  var result = [];
+
+  for (var i = 0; i < arr.length; i += 1) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i += 1;
+        break;
+      case "--discard-prev":
+        if (result.length != 0) result.pop();
+        break;
+      case "--double-next":
+        if (i < arr.length - 1) result.push(arr[i + 1]);
+        break;
+      case "--double-prev":
+        if (i >= 1) result.push(arr[i - 1]);
+        break;
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
+  
 };
