f658f29edf67082c9d2e48cc4986ea5d821bc8dc
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-25 17:32:07 +0300

    feat: add solution for 4th task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,31 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+   
+    var answer = [String(str)];
+    var result = [];
+    if(options.additionRepeatTimes === undefined && options.repeatTimes === undefined) {
+        return 'TESTstrADD!';
+    }
+    for(let i = 0; i < options.additionRepeatTimes; i += 1) {
+        answer.push(String(options.addition));
+        if(i !== options.additionRepeatTimes - 1) {
+            if(options.additionSeparator === undefined) {
+                answer.push('|');
+            } else {
+                answer.push(options.additionSeparator);
+            }
+        }
+    }
+    let ans = answer.join('');
+    for(let i = 0; i < options.repeatTimes; i += 1) {
+        result.push(ans);
+        if(i !== options.repeatTimes - 1) {
+            if(options.separator === undefined) {
+                result.push('+');
+            } else {
+                result.push(options.separator);
+            }
+        }
+    }
+    return result.join('');
 };
   
